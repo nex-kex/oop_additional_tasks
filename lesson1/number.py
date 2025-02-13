da2c62bcc4676abb062ee4d0f99122c030687cfa
Mark Volkov <markasch67@gmail.com>
2025-02-13 13:38:50 +0300

    Lesson 1

--- lesson1/number.py
+++ lesson1/number.py
@@ -9,22 +9,24 @@
 
 `.add(<значение>)` добавляет указанное число к value
 
-`.substract(<значение>)` вычитает указанное число из value
+`.subtract(<значение>)` вычитает указанное число из value
 """
 
 class Number:
 
     def __init__(self, value):
-        ...
+        self.value = value
 
-    def ...(self):
-        ...
+    def get(self):
+        return self.value
 
-    def ...:
-        ...
+    def add(self, addition):
+        self.value += addition
+        return self.value
 
-    def ...:
-        ...
+    def subtract(self, subtraction):
+        self.value -= subtraction
+        return self.value
 
 
 # код для проверки 
@@ -32,5 +34,5 @@ n = Number(7)
 print(n.get())  # 7
 n.add(3)
 print(n.get())  # 10
-n.substract(5)
+n.subtract(5)
 print(n.get())  # 5
